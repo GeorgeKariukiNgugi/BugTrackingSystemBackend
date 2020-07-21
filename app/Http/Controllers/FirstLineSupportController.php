ce153f0265eeada7c80ea065fc876f934f42a921
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-07-21 10:02:06 +0300

    Added the reourec Controllers For the application

---
+++ app/Http/Controllers/FirstLineSupportController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\FirstLineSupport;
+use Illuminate\Http\Request;
+
+class FirstLineSupportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\FirstLineSupport  $firstLineSupport
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FirstLineSupport $firstLineSupport)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\FirstLineSupport  $firstLineSupport
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(FirstLineSupport $firstLineSupport)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\FirstLineSupport  $firstLineSupport
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, FirstLineSupport $firstLineSupport)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\FirstLineSupport  $firstLineSupport
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(FirstLineSupport $firstLineSupport)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/LeadApprovalController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\LeadApproval;
+use Illuminate\Http\Request;
+
+class LeadApprovalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\LeadApproval  $leadApproval
+     * @return \Illuminate\Http\Response
+     */
+    public function show(LeadApproval $leadApproval)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\LeadApproval  $leadApproval
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(LeadApproval $leadApproval)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\LeadApproval  $leadApproval
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, LeadApproval $leadApproval)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\LeadApproval  $leadApproval
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(LeadApproval $leadApproval)
+    {
+        //
+    }
+}
